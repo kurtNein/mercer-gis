e1d132a906f91c4c262d83d89a6655ebb0fb484e
kurtNein <109308607+kurtNein@users.noreply.github.com>
2023-02-07 13:08:20 -0500

    Update buffer-site-code.js

--- buffer-site-code.js
+++ buffer-site-code.js
@@ -1,3 +1,6 @@
+// If a new feature is within a distance (geodetic) of another feature, and the other feature has a value for this field,
+// The new feature will copy that value to its same field. Otherwise, it returns the number zero.
+
 var siteBuffer = BufferGeodetic($feature, 100, 'yards')
 
 var siteContains = First(Contains(siteBuffer, FeatureSetByName($datastore,"Surveillance_Data")))
