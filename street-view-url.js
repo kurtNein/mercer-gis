03959066381caf1d713677b169362e6987b84702
Kurt Neinstedt <109308607+kurtNein@users.noreply.github.com>
2024-06-05 16:06:12 -0400

    Update street-view-url.js

--- street-view-url.js
+++ street-view-url.js
@@ -1,7 +1,11 @@
-var base_url = 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint='
+var base_url = 'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=';
 
-var lat = Text($feature.LATITUDE)
-var lon = Text($feature.LONGITUDE)
+if (IsEmpty(Geometry($feature))) {
+return 'None'
+}
+
+var lon = Text(Round(Geometry($feature).x, 6));
+var lat = Text(Round(Geometry($feature).y, 6));
 
 var full_url = base_url + lat + ',' + lon
 
